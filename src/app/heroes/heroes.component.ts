81bf76e8b42c5e6ed9bf56b7e51e6b8f667cc97c
samriz <samriz@users.noreply.github.com>
2022-10-28 15:38:30 -0500

    Update heroes.component.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
 
+//component decorator for Heroes Component class
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit 
+export class HeroesComponent implements OnInit //component class
 {
   hero: Hero = {
     id: 1,
