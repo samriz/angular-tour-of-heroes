3c2ce491205850bad4c0fc18a4fdf684687ad1ea
samriz <samriz@users.noreply.github.com>
2022-11-04 16:23:36 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+//tell the Router which view to display when a user clicks a link or pastes a URL into the browser address bar
+//path: string that matches the URL in the browser address bar
+//component: component that the router should create when navigating to this route
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
 
+//@NgModule metadata initializes the router and starts it listening for browser location changes
 @NgModule({
+  //adds the RouterModule to the AppRoutingModule imports array and configures it with the routes in one step by calling RouterModule.forRoot()
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] //AppRoutingModule exports RouterModule to be available throughout the application
 })
 export class AppRoutingModule { }
