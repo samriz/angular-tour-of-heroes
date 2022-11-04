d705df38c232a44365262b0caee2a4addd1cd9cb
samriz <samriz@users.noreply.github.com>
2022-11-04 17:08:13 -0500

    Added a dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 //tell the Router which view to display when a user clicks a link or pastes a URL into the browser address bar
 //path: string that matches the URL in the browser address bar
 //component: component that the router should create when navigating to this route
-const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },  
+];
 
 //@NgModule metadata initializes the router and starts it listening for browser location changes
 @NgModule({
